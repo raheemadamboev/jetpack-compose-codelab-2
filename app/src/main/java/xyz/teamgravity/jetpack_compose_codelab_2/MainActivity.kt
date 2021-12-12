05065716a6c620add9c891a768dbcf21e2705107
raheemadamboev <raheemideadev@gmail.com>
2021-12-13 03:11:25 +0500

    working with lists

--- app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_2/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/jetpack_compose_codelab_2/MainActivity.kt
@@ -3,12 +3,12 @@ package xyz.teamgravity.jetpack_compose_codelab_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,11 +16,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
 import xyz.teamgravity.jetpack_compose_codelab_2.ui.theme.Jetpackcomposecodelab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Jetpackcomposecodelab2Theme {
                 Surface(color = MaterialTheme.colors.background) {
-                    LayoutsCodeLab()
+                    SimpleList()
                 }
             }
         }
@@ -92,16 +95,78 @@ private fun LayoutsCodeLab() {
 
         }
     ) { innerPadding ->
-        BodyContent(modifier = Modifier
-            .padding(innerPadding)
-            .padding(8.dp))
+        BodyContent(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
+private fun BodyContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(text = "Hi there!")
         Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Composable
+private fun SimpleList() {
+    Column {
+
+        val listSize = 100
+        // save the scrolling position with this state
+        val scrollState = rememberLazyListState()
+        // save the coroutine scope where our animated scroll will be executed
+        val coroutineScope = rememberCoroutineScope()
+
+        Row {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        // 0 is the first item index
+                        scrollState.animateScrollToItem(0)
+                    }
+                }
+            ) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        // listSize - 1 is the last index of the list
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }
+            ) {
+                Text(text = "Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = "Item #$index",
+            style = MaterialTheme.typography.subtitle1
+        )
     }
 }
